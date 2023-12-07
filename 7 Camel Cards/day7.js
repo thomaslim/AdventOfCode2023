757a7677fc9ece2d00f09d0f5e35a7e842c77cde
Thomas Lim <letscode@tlim.fr>
2023-12-07 14:26:42 +0100

    Challenge 7.2

--- 7 Camel Cards/day7.js
+++ 7 Camel Cards/day7.js
@@ -1,91 +1,106 @@
+function convertCardToValue(card, withJokerRule = false) {
+  if (card === 'T') return 10;
+  if (card === 'J' && withJokerRule) return 1;
+  if (card === 'J' && !withJokerRule) return 11;
+  // if (card === 'J') return 11;
+  if (card === 'Q') return 12;
+  if (card === 'K') return 13;
+  if (card === 'A') return 14;
+  return +card;
+}
+
+function splitSet(inputString) {
+  const lines = inputString.split('\n');
+
+  const setArray = lines.reduce((acc, line) => {
+    const lineParts = line.split(' ');
+    const hand = [
+      lineParts[0][0],
+      lineParts[0][1],
+      lineParts[0][2],
+      lineParts[0][3],
+      lineParts[0][4]];
+    const bid = +lineParts[1];
+    acc.push({ hand, bid });
+    return acc;
+  }, []);
+
+  return setArray;
+}
+
+function getTypeOfHand(hand, withJokerRule = false) {
+  const table = {};
+  let value;
+  // compute nb of occurence per value
+  for (let i = 0; i < hand.length; i += 1) {
+    value = hand[i];
+    if (!table[value]) table[value] = 1;
+    else table[value] += 1;
+  }
+
+  if (withJokerRule && table.J && table.J !== 5) {
+    const numberOfJ = table.J;
+    delete table.J;
+    const maxKey = Object.keys(table).reduce((a, b) => (table[a] > table[b] ? a : b));
+    table[maxKey] += numberOfJ;
+  }
+
+  const sizeOfTable = Object.keys(table).length;
+  if (sizeOfTable === 1) return '7_5OAK'; // Five of a kind
+  if (sizeOfTable === 2) {
+    if (Object.values(table).includes(4)) return '6_4OAK';
+    return '5_FH';
+  }
+  if (sizeOfTable === 3) {
+    if (Object.values(table).includes(3)) return '4_3OAK';
+    return '3_2P';
+  }
+  if (sizeOfTable === 4) return '2_1P';
+
+  return '1_HC';
+}
+
+function parseHandsAndAddType(hands, withJokerRule = false) {
+  return hands.map((hand) => ({ ...hand, type: getTypeOfHand(hand.hand, withJokerRule) }));
+}
+
+function sortHands(hands, withJokerRule = false) {
+  const handsWithType = parseHandsAndAddType(hands, withJokerRule);
+  const sortedHands = handsWithType.sort((a, b) => {
+    if (a.type < b.type) return -1;
+    if (a.type > b.type) return 1;
+
+    // both hands are the same type, look at values
+    const handAvalues = a.hand.map((item) => convertCardToValue(item, withJokerRule));
+    const handBvalues = b.hand.map((item) => convertCardToValue(item, withJokerRule));
+    for (let i = 0; i < 5; i += 1) {
+      if (handAvalues[i] < handBvalues[i]) return -1;
+      if (handAvalues[i] > handBvalues[i]) return 1;
+    }
+    return 0;
+  });
+
+  return sortedHands;
+}
+
+function getTotalWinnings(hands, withJokerRule = false) {
+  const sortedHands = sortHands(hands, withJokerRule);
+  const result = sortedHands.reduce((acc, item, index) => acc + item.bid * (index + 1), 0);
+  return result;
+}
+
+function testHandWithJokerRule(hand) {
+  if (!hand.includes('J')) return getTypeOfHand(hand);
+
+  return getTypeOfHand(hand, true);
+}
+
 module.exports = {
-    splitSet,
-    getTypeOfHand,
-    parseHandsAndAddType,
-    sortHands,
-    getTotalWinnings
-}
-
-function convertCardToValue (card) {
-    if (card == "T") return 10
-    if (card == "J") return 11
-    if (card == "Q") return 12
-    if (card == "K") return 13
-    if (card == "A") return 14
-    return +card
-}
-
-function splitSet (inputString) {
-    const lines = inputString.split('\n')
-
-    const setArray = lines.reduce((acc, line) => {
-        const lineParts = line.split(" ")
-        const hand = [ lineParts[0][0], lineParts[0][1], lineParts[0][2], lineParts[0][3], lineParts[0][4]]//.map(convertCardToValue)
-        const bid = +lineParts[1]
-        acc.push({hand, bid})
-        return acc 
-    }, [])
-
-    return setArray
-}
-
-function getTypeOfHand (hand) {
-    const table = {}
-    let value
-    for (let i = 0; i<hand.length; i++) {
-        value = hand [i]
-        if (!table[value]) table[value] = 1
-        else table[value]++
-    }
-
-    const sizeOfTable = Object.keys(table).length
-    if (sizeOfTable == 1 ) return "7_5OAK" // Five of a kind
-    if (sizeOfTable == 2 ) {
-        if (Object.values(table).includes(4)) return "6_4OAK"
-        return "5_FH"
-    }
-    if (sizeOfTable == 3 ) {
-        if (Object.values(table).includes(3)) return "4_3OAK"
-        return "3_2P" 
-    }
-    if (sizeOfTable == 4 ) return "2_1P"
-   
-    return "1_HC"
-}
-
-function parseHandsAndAddType(hands) {
-    return hands.map (hand => {
-        hand.type = getTypeOfHand(hand.hand)
-        return hand
-    })
-}
-
-function sortHands (hands) {
-    const handsWithType = parseHandsAndAddType(hands)
-    const sortedHands = handsWithType.sort((a, b) => {
-        if (a.type < b.type) return -1
-        if (a.type > b.type) return 1
-
-        // both hands are the same type, look at values
-        const handAvalues = a.hand.map(convertCardToValue)
-        const handBvalues = b.hand.map(convertCardToValue)
-        for( let i = 0; i < 5; i++) {
-            if (handAvalues[i] < handBvalues[i]) return -1
-            if (handAvalues[i] > handBvalues[i]) return 1
-        }
-        return 0
-    })
-
-  return sortedHands
-}
-
-function getTotalWinnings(hands){
-    let sortedHands = sortHands(hands)
-    // for (i=1 ; i<2000; i++) {
-    //     sortedHands = sortHands(sortedHands)
-    // }
-    const result = sortedHands.reduce((acc, item, index) => {
-          return acc + item.bid * (index +1)
-          },0)
-    return result
-}
+  splitSet,
+  getTypeOfHand,
+  parseHandsAndAddType,
+  sortHands,
+  getTotalWinnings,
+  testHandWithJokerRule,
+  convertCardToValue,
+};
--- 7 Camel Cards/day7.test.js
+++ 7 Camel Cards/day7.test.js
@@ -1,64 +1,79 @@
-const functions = require('./day7')
+const fs = require('fs');
+const functions = require('./day7');
 
 const demoSet = `32T3K 765
 T55J5 684
 KK677 28
 KTJJT 220
-QQQJA 483`
+QQQJA 483`;
 
-const fs = require('fs');
-const puzzleInput = fs.readFileSync('7 Camel Cards/input.txt' ).toString('utf-8').trim();
+const puzzleInput = fs.readFileSync('7 Camel Cards/input.txt').toString('utf-8').trim();
 
-test('Read input', ()=>{
-    expect(functions.splitSet(demoSet).length).toEqual(5)
-    expect(functions.splitSet(demoSet)[0]).toEqual({"bid": 765, "hand": ["3", "2", "T", "3", "K"]})
-})
+test('Read input', () => {
+  expect(functions.splitSet(demoSet).length).toEqual(5);
+  expect(functions.splitSet(demoSet)[0]).toEqual({ bid: 765, hand: ['3', '2', 'T', '3', 'K'] });
+});
 
 test('get type of hand', () => {
-    expect(functions.getTypeOfHand([5,5,5,5,5])).toEqual('7_5OAK')
-    expect(functions.getTypeOfHand([5,5,5,2,5])).toEqual('6_4OAK')
-    expect(functions.getTypeOfHand([5,2,5,2,5])).toEqual('5_FH')
-    expect(functions.getTypeOfHand(["T","2","T","A","T"])).toEqual('4_3OAK')
-    expect(functions.getTypeOfHand(["T","2","T","A","A"])).toEqual('3_2P')
-    expect(functions.getTypeOfHand(["3", "2", "2", "A", "T"])).toEqual('2_1P')
+  expect(functions.getTypeOfHand([5, 5, 5, 5, 5])).toEqual('7_5OAK');
+  expect(functions.getTypeOfHand([5, 5, 5, 2, 5])).toEqual('6_4OAK');
+  expect(functions.getTypeOfHand([5, 2, 5, 2, 5])).toEqual('5_FH');
+  expect(functions.getTypeOfHand(['T', '2', 'T', 'A', 'T'])).toEqual('4_3OAK');
+  expect(functions.getTypeOfHand(['T', '2', 'T', 'A', 'A'])).toEqual('3_2P');
+  expect(functions.getTypeOfHand(['3', '2', '2', 'A', 'T'])).toEqual('2_1P');
 
-    expect(functions.getTypeOfHand(["4", "2", "T", "3", "K"])).toEqual('1_HC')
-})
+  expect(functions.getTypeOfHand(['4', '2', 'T', '3', 'K'])).toEqual('1_HC');
+});
 
 test('add type of hand to hands array', () => {
-    expect(functions.parseHandsAndAddType(
-        functions.splitSet(demoSet)
-    )[0]).toEqual({"bid": 765, "hand": ["3", "2", "T", "3", "K"], type: "2_1P"})
-})
+  expect(functions.parseHandsAndAddType(
+    functions.splitSet(demoSet),
+  )[0]).toEqual({ bid: 765, hand: ['3', '2', 'T', '3', 'K'], type: '2_1P' });
+});
 
 test('sort hands ', () => {
-    // expect(functions.sortHands(functions.splitSet(demoSet))).toEqual([
-    //     { hand: [ '3', '2', 'T', '3', 'K' ], bid: 765, type: '2_1P' },
-    //     { hand: [ 'K', 'T', 'J', 'J', 'T' ], bid: 220, type: '3_2P' },
-    //     { hand: [ 'K', 'K', '6', '7', '7' ], bid: 28, type: '3_2P' },
-    //     { hand: [ 'T', '5', '5', 'J', '5' ], bid: 684, type: '4_3OAK' },
-    //     { hand: [ 'Q', 'Q', 'Q', 'J', 'A' ], bid: 483, type: '4_3OAK' }
-    //   ])
+  expect(functions.sortHands([
+    { hand: ['K', '2', '7', '5', '8'], type: '1_HC' },
+    { hand: ['Q', 'Q', 'Q', 'J', 'A'], type: '4_3OAK' },
+    { hand: ['K', '2', '7', '5', '6'], type: '1_HC' },
+    { hand: ['K', 'K', '6', '7', '7'], type: '3_2P' },
+    { hand: ['T', 'K', '4', '8', '6'], type: '1_HC' },
+    { hand: ['T', '5', '5', 'J', '5'], type: '4_3OAK' },
+  ])).toStrictEqual([
+    { hand: ['T', 'K', '4', '8', '6'], type: '1_HC' },
+    { hand: ['K', '2', '7', '5', '6'], type: '1_HC' },
+    { hand: ['K', '2', '7', '5', '8'], type: '1_HC' },
+    { hand: ['K', 'K', '6', '7', '7'], type: '3_2P' },
+    { hand: ['T', '5', '5', 'J', '5'], type: '4_3OAK' },
+    { hand: ['Q', 'Q', 'Q', 'J', 'A'], type: '4_3OAK' },
 
-      expect(functions.sortHands([
-        { hand: ['K', '2', '7', '5', '8'], type: '1_HC' },
-        { hand: [ 'Q', 'Q', 'Q', 'J', 'A' ], type: '4_3OAK' },
-        { hand: ['K', '2', '7', '5', '6'], type: '1_HC' },
-        { hand: [ 'K', 'K', '6', '7', '7' ], type: '3_2P' },
-        { hand: ['T', 'K', '4', '8', '6'], type: '1_HC' },
-        { hand: [ 'T', '5', '5', 'J', '5' ], type: '4_3OAK' },
-      ])).toStrictEqual([
-        { hand: ['T', 'K', '4', '8', '6'], type: '1_HC' },
-        { hand: ['K', '2', '7', '5', '6'], type: '1_HC' },
-        { hand: ['K', '2', '7', '5', '8'], type: '1_HC' },
-        { hand: [ 'K', 'K', '6', '7', '7' ], type: '3_2P' },
-        { hand: [ 'T', '5', '5', 'J', '5' ], type: '4_3OAK' },
-        { hand: [ 'Q', 'Q', 'Q', 'J', 'A' ], type: '4_3OAK' },
-       
-      ])
-})
+  ]);
+});
 
-test('get score', () => {
-    expect(functions.getTotalWinnings(functions.splitSet(demoSet))).toEqual(6440)
-    expect(functions.getTotalWinnings(functions.splitSet(puzzleInput))).toEqual(249726565)
-})
+test('get score part 1', () => {
+  expect(functions.getTotalWinnings(functions.splitSet(demoSet))).toEqual(6440);
+  expect(functions.getTotalWinnings(functions.splitSet(puzzleInput))).toEqual(249726565);
+});
+
+test('get type of hand with Joker rule', () => {
+  expect(functions.testHandWithJokerRule([5, 5, 5, 5, 5])).toEqual('7_5OAK');
+  expect(functions.testHandWithJokerRule([5, 2, 5, 2, 5])).toEqual('5_FH');
+  expect(functions.testHandWithJokerRule(['T', '2', 'T', 'A', 'A'])).toEqual('3_2P');
+  expect(functions.testHandWithJokerRule(['3', '2', '2', 'A', 'T'])).toEqual('2_1P');
+
+  expect(functions.testHandWithJokerRule(['J', 'T', 'T', 'J', 'T'])).toEqual('7_5OAK');
+  expect(functions.testHandWithJokerRule([5, 5, 'J', 2, 5])).toEqual('6_4OAK');
+  expect(functions.testHandWithJokerRule(['T', '2', 'J', 'A', 'T'])).toEqual('4_3OAK');
+  expect(functions.testHandWithJokerRule(['J', 'J', '9', 'J', 'Q'])).toEqual('6_4OAK');
+  expect(functions.testHandWithJokerRule(['4', '2', 'T', '3', 'K'])).toEqual('1_HC');
+});
+
+test('convert Cart', () => {
+  expect(functions.convertCardToValue('J', true)).toEqual(1);
+  expect(functions.convertCardToValue('J')).toEqual(11);
+});
+
+test('get score part 2', () => {
+  expect(functions.getTotalWinnings(functions.splitSet(demoSet), true)).toEqual(5905);
+  expect(functions.getTotalWinnings(functions.splitSet(puzzleInput), true)).toEqual(251135960);
+});
