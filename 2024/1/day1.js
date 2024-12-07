3a455a2ff911ecf439ce28a72c288a2365b68e64
Thomas Lim <letscode@tlim.fr>
2024-12-07 15:12:34 +0100

    2024 day 1, part 1

---
+++ 2024/1/day1.js
@@ -0,0 +1,39 @@
+// https://adventofcode.com/2024/day/1
+
+function parseInputAsString(input) {
+  const result = input.split("\n").map((line) => line.split("   ")).reduce((acc, line, index) => {
+    acc.column1.push(parseInt(line[0]))
+    acc.column2.push(parseInt(line[1]))
+
+    return acc
+  }, {column1 : [], column2 : []});
+  return result
+}
+
+function sortObject(input) {
+  const sortColumn1 = input.column1.sort((a, b) => a-b)
+  const sortColumn2 = input.column2.sort((a, b) => a-b)
+  return {column1:sortColumn1, column2: sortColumn2 }
+
+}
+
+
+
+function calculateDistance(i, j){
+  return j<i ?  i-j : j-i
+}
+
+function computeTotalDistance(sortedObject) {
+  if (sortedObject.column1.length != sortedObject.column2.length) {
+    throw new Error("columns are not the same size")
+  }
+  const totalDistance = sortedObject.column1.reduce((acc, item, index) => acc + calculateDistance(sortedObject.column1[index], sortedObject.column2[index]), 0)
+  return totalDistance
+}
+
+module.exports = {
+  parseInputAsString,
+  sortObject,
+  calculateDistance,
+  computeTotalDistance
+};
---
+++ 2024/1/day1.test.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const f = require("./day1.js");
+
+const puzzleInput = fs.readFileSync("2024/1/input.txt").toString("utf-8").trim();
+
+const demoInput1 = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
+
+test("Parse input string", () => {
+  expect(typeof f.parseInputAsString(demoInput1)).toBe("object");
+  expect(f.parseInputAsString(demoInput1).column1.length).toEqual(6);
+  expect(f.parseInputAsString(demoInput1).column2.length).toEqual(6);
+  expect(f.parseInputAsString(puzzleInput).column1.length).toEqual(1000);
+  expect(f.parseInputAsString(puzzleInput).column2.length).toEqual(1000);
+});
+
+test("Sort object", () => {
+  const parsedInput = f.parseInputAsString(demoInput1)
+  const result = f.sortObject(parsedInput)
+  expect(result.column1).toEqual([1,2,3,3,3, 4])
+  expect(result.column2).toEqual([3,3,3,4,5,9])
+})
+
+test("Calculate Distance", () => {
+  expect (f.calculateDistance(3,3)).toEqual(0)
+  expect (f.calculateDistance(1,3)).toEqual(2)
+})
+
+test("Compute Total Distance", () => {
+  const parsedInput = f.parseInputAsString(demoInput1)
+  const result = f.sortObject(parsedInput)
+  expect(f.computeTotalDistance(result)).toEqual(11)
+})
+
+test("Solution", () => {
+  const parsedInput = f.parseInputAsString(puzzleInput)
+  const result = f.sortObject(parsedInput) 
+  expect(f.computeTotalDistance(result)).toEqual(11)
+})
