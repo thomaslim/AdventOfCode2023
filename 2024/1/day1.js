dece85fdb3d5b7f69a0a030edf8b0638ce8a95e6
Thomas Lim <letscode@tlim.fr>
2024-12-07 15:29:26 +0100

    Lint + Pretty Day 1

--- 2024/1/day1.js
+++ 2024/1/day1.js
@@ -1,41 +1,58 @@
 // https://adventofcode.com/2024/day/1
 
 function parseInputAsString(input) {
-  const result = input.split("\n").map((line) => line.split("   ")).reduce((acc, line, index) => {
-    acc.column1.push(parseInt(line[0]))
-    acc.column2.push(parseInt(line[1]))
+  const result = input
+    .split("\n")
+    .map((line) => line.split("   "))
+    .reduce(
+      (acc, line) => {
+        acc.column1.push(parseInt(line[0], 10));
+        acc.column2.push(parseInt(line[1], 10));
 
-    return acc
-  }, {column1 : [], column2 : []});
-  return result
+        return acc;
+      },
+      { column1: [], column2: [] },
+    );
+  return result;
 }
 
 function sortObject(input) {
-  const sortColumn1 = input.column1.sort((a, b) => a-b)
-  const sortColumn2 = input.column2.sort((a, b) => a-b)
-  return {column1:sortColumn1, column2: sortColumn2 }
-
+  const sortColumn1 = input.column1.sort((a, b) => a - b);
+  const sortColumn2 = input.column2.sort((a, b) => a - b);
+  return { column1: sortColumn1, column2: sortColumn2 };
 }
 
-function calculateDistance(i, j){
-  return j<i ?  i-j : j-i
+function calculateDistance(i, j) {
+  return j < i ? i - j : j - i;
 }
 
 function computeTotalDistance(sortedObject) {
-  if (sortedObject.column1.length != sortedObject.column2.length) {
-    throw new Error("columns are not the same size")
+  if (sortedObject.column1.length !== sortedObject.column2.length) {
+    throw new Error("columns are not the same size");
   }
-  const totalDistance = sortedObject.column1.reduce((acc, item, index) => acc + calculateDistance(sortedObject.column1[index], sortedObject.column2[index]), 0)
-  return totalDistance
+  const totalDistance = sortedObject.column1.reduce(
+    (acc, item, index) =>
+      acc +
+      calculateDistance(
+        sortedObject.column1[index],
+        sortedObject.column2[index],
+      ),
+    0,
+  );
+  return totalDistance;
 }
 
 function findSimilarityForNumber(item, array) {
-  const filteredArray = array.filter(i => i === item)
-  return filteredArray.length
+  const filteredArray = array.filter((i) => i === item);
+  return filteredArray.length;
 }
 
-function findTotalSimilarity (sortedObject) {
-  return sortedObject.column1.reduce( (acc, item) =>  acc + item * findSimilarityForNumber(item,sortedObject.column2 ), 0)
+function findTotalSimilarity(sortedObject) {
+  return sortedObject.column1.reduce(
+    (acc, item) =>
+      acc + item * findSimilarityForNumber(item, sortedObject.column2),
+    0,
+  );
 }
 
 module.exports = {
--- 2024/1/day1.test.js
+++ 2024/1/day1.test.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const f = require("./day1.js");
 
-const puzzleInput = fs.readFileSync("2024/1/input.txt").toString("utf-8").trim();
+const puzzleInput = fs
+  .readFileSync("2024/1/input.txt")
+  .toString("utf-8")
+  .trim();
 
 const demoInput1 = `3   4
 4   3
@@ -9,7 +12,6 @@ const demoInput1 = `3   4
 1   3
 3   9
 3   3`;
-
 
 test("Parse input string", () => {
   expect(typeof f.parseInputAsString(demoInput1)).toBe("object");
@@ -20,42 +22,42 @@ test("Parse input string", () => {
 });
 
 test("Sort object", () => {
-  const parsedInput = f.parseInputAsString(demoInput1)
-  const result = f.sortObject(parsedInput)
-  expect(result.column1).toEqual([1,2,3,3,3, 4])
-  expect(result.column2).toEqual([3,3,3,4,5,9])
-})
+  const parsedInput = f.parseInputAsString(demoInput1);
+  const result = f.sortObject(parsedInput);
+  expect(result.column1).toEqual([1, 2, 3, 3, 3, 4]);
+  expect(result.column2).toEqual([3, 3, 3, 4, 5, 9]);
+});
 
 test("Calculate Distance", () => {
-  expect (f.calculateDistance(3,3)).toEqual(0)
-  expect (f.calculateDistance(1,3)).toEqual(2)
-})
+  expect(f.calculateDistance(3, 3)).toEqual(0);
+  expect(f.calculateDistance(1, 3)).toEqual(2);
+});
 
 test("Compute Total Distance", () => {
-  const parsedInput = f.parseInputAsString(demoInput1)
-  const result = f.sortObject(parsedInput)
-  expect(f.computeTotalDistance(result)).toEqual(11)
-})
+  const parsedInput = f.parseInputAsString(demoInput1);
+  const result = f.sortObject(parsedInput);
+  expect(f.computeTotalDistance(result)).toEqual(11);
+});
 
 test("Solution", () => {
-  const parsedInput = f.parseInputAsString(puzzleInput)
-  const result = f.sortObject(parsedInput) 
-  expect(f.computeTotalDistance(result)).toEqual(1970720)
-})
+  const parsedInput = f.parseInputAsString(puzzleInput);
+  const result = f.sortObject(parsedInput);
+  expect(f.computeTotalDistance(result)).toEqual(1970720);
+});
 
 test("find similarity", () => {
-  const parsedInput = f.parseInputAsString(demoInput1)
-  expect(f.findSimilarityForNumber(3, parsedInput.column2)).toEqual(3)
-  expect(f.findSimilarityForNumber(4, parsedInput.column2)).toEqual(1)
-  expect(f.findSimilarityForNumber(2, parsedInput.column2)).toEqual(0)
-})
+  const parsedInput = f.parseInputAsString(demoInput1);
+  expect(f.findSimilarityForNumber(3, parsedInput.column2)).toEqual(3);
+  expect(f.findSimilarityForNumber(4, parsedInput.column2)).toEqual(1);
+  expect(f.findSimilarityForNumber(2, parsedInput.column2)).toEqual(0);
+});
 
 test("find total similarity ", () => {
-  const parsedInput = f.parseInputAsString(demoInput1)
-  expect(f.findTotalSimilarity(parsedInput)).toEqual(31)
-})
+  const parsedInput = f.parseInputAsString(demoInput1);
+  expect(f.findTotalSimilarity(parsedInput)).toEqual(31);
+});
 
 test("Solution part 2 ", () => {
-  const parsedInput = f.parseInputAsString(puzzleInput)
-  expect(f.findTotalSimilarity(parsedInput)).toEqual(17191599)
-})
+  const parsedInput = f.parseInputAsString(puzzleInput);
+  expect(f.findTotalSimilarity(parsedInput)).toEqual(17191599);
+});
