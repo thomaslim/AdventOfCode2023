8821cc63e19bc134c9a9f979b76b617d320fd824
Thomas Lim <letscode@tlim.fr>
2024-12-07 14:01:47 +0100

    Move 2023 files to 2023 folder

---
+++ 2023/19/test.js
@@ -0,0 +1,26 @@
+import assert from 'node:assert';
+import test  from 'node:test';
+
+const myFunct = (input) => {
+  const splittedInput = input.split('')
+  return splittedInput
+}
+
+function myFunct2 (input){
+  const splittedInput = input.split('')
+  return splittedInput
+}
+
+
+test('shouldShould equal', () => {
+  assert.deepEqual(myFunct("bonjour"), [
+    'b', 'o', 'n',
+    'j', 'o', 'u',
+    'r'
+  ]);
+  assert.deepEqual(myFunct2("bonjour"), [
+    'b', 'o', 'n',
+    'j', 'o', 'u',
+    'r'
+  ]);
+});
